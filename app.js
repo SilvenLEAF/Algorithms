d7dcd6a3b574b628384b68e75756c8218172defd
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-03 09:33:47 +0530

    Valid Parentheses

--- app.js
+++ app.js
@@ -1,46 +1,55 @@
-// ALGORITHMS 1
 
 
-
-/* 
-Create a function that takes an array of numbers as its first argument and a target number as its second argument,
-There will be only two numbers in that array that add up to the target number, return their indices as an array
-by that created function, also the number and its complement number must not be the same number with thesame index
+const isValid = (s)=>{
+  let stack = [];
 
 
-Your approach MUST be
-TIME COMPLEXITY: O(n)
-SPACE COMPLEXITY: O(n)
+  for(let i=0; i<stack.length; i++){
+    if(s[i] === '(' || s[i] === '{' || s[i] === '['){
+    
+      stack.push(s[i]);
+    
+    } else {
 
-*/
+      if(stack.length === 0) return false;
+      let pair = stack.pop() + s[i];
 
-
-const twoSum = (nums, target) => { // nums is an array of numbers, target is a single number
-  
-  
-  const map = new Map(); // creating a new Map object in JavaScript
-  
-
-  const numsLength = nums.length;
-
-  for(let i = 0; i<numsLength; i++) { // doing a liner search, iterating through all the numbers in that nums
-      
-    const num = nums[i]; // storing the number inside num
-
-
-      if(map.get(num) === undefined) map.set(target-num, i); // first checking if its complement number's index is already inside our map, if not, save it's index for its complement  number
-      else return [map.get(num), i]; // if yes, return its complement number's index and its index
+      if(pair !== '()' && pair !== '{}' && pair !== '[]'){
+        return false;
+      }
+    }
   }
-};
+
+  return stack.length === 0;
+}
+
+
+
+// const isValid = (s)=>{
+//   let stack = [];
+  
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
+//       stack.push(s[i]);
+//     } 
+//     else {
+//       if (stack.length === 0) return false;
+//       let pair = stack.pop() + s[i];
+//       if (pair !== '()' && pair !== '{}' && pair !== '[]') {
+//          return false;
+//       }
+//     }
+//   }
+  
+//   return stack.length === 0;  
+// }
 
 
 
 
 
 
-// Testing the function
-const res = twoSum([2,7,11,15], 9);
-
+const res = isValid("{[]}");
 
 // OUTPUTTING the TEST result
 console.log(`%c Result is [${  res }]`, `color: red; font-size: 150%`);
