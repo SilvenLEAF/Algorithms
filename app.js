f2e0f6abf48191dbc852830bf6957c7a818b894a
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-03 09:42:49 +0530

    refactored

--- app.js
+++ app.js
@@ -1,48 +1,37 @@
+// ALGORITHMS 2 (Valid Parentheses)
 
 
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+.     Open brackets must be closed by the same type of brackets.
+.     Open brackets must be closed in the correct order.
+
+
+Your approach MUST be
+TIME COMPLEXITY: O(n)
+SPACE COMPLEXITY: O(n)
+
+*/
 const isValid = (s)=>{
   let stack = [];
-
-
-  for(let i=0; i<stack.length; i++){
-    if(s[i] === '(' || s[i] === '{' || s[i] === '['){
-    
+  
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
       stack.push(s[i]);
-    
-    } else {
-
-      if(stack.length === 0) return false;
+    } 
+    else {
+      if (stack.length === 0) return false;
       let pair = stack.pop() + s[i];
-
-      if(pair !== '()' && pair !== '{}' && pair !== '[]'){
-        return false;
+      if (pair !== '()' && pair !== '{}' && pair !== '[]') {
+         return false;
       }
     }
   }
-
-  return stack.length === 0;
+  
+  return stack.length === 0;  
 }
-
-
-
-// const isValid = (s)=>{
-//   let stack = [];
-  
-//   for (let i = 0; i < s.length; i++) {
-//     if (s[i] === '(' || s[i] === '{' || s[i] === '[') {
-//       stack.push(s[i]);
-//     } 
-//     else {
-//       if (stack.length === 0) return false;
-//       let pair = stack.pop() + s[i];
-//       if (pair !== '()' && pair !== '{}' && pair !== '[]') {
-//          return false;
-//       }
-//     }
-//   }
-  
-//   return stack.length === 0;  
-// }
 
 
 
