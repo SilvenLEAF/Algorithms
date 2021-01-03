1d0155bce5bcc468a2fa5e9d24ccb2b25c3f6b43
SilvenLEAF <SilvenLEAF@gmail.com>
2021-01-03 09:45:59 +0530

    resolved merge  error

--- app.js
+++ app.js
@@ -42,10 +42,4 @@ const res = isValid("{[]}");
 
 // OUTPUTTING the TEST result
 console.log(`%c Result is [${  res }]`, `color: orangered; font-size: 150%`);
-console.log(`%c Result is [${  res1 }]`, `color: green; font-size: 150%`);
-console.log(`%c Result is [${  res2 }]`, `color: blue; font-size: 150%`);
-console.log(`%c Result is [${  res3 }]`, `color: black; font-size: 150%`);
-console.log(`%c Result is [${  res4 }]`, `color: orange; font-size: 150%`);
-console.log(`%c Result is [${  res5 }]`, `color: red; font-size: 150%`);
-
 
